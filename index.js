8e26dc0926be511976968c7d34a7f6a1ba60dc5a
William Huang <william2958@gmail.com>
2020-06-04 17:45:52 -0400

    Initial Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const config = require('./config');
+const api = require('./src/api');
+
+const app = express();
+const cors = require('cors');
+
+const port = 8000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/', api(config));
+
+app.listen(port, () => console.log(`App is listening on ${port}`));
