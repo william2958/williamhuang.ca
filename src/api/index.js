fdec67c35b3792a126559253425f34e80c93334e
William Huang <william2958@gmail.com>
2020-06-04 20:00:08 -0400

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const admin = require('firebase-admin');
+const serviceAccount = require('../../ServiceAccountKey.json');
+
+let credential;
+
+if (process.env.NODE_ENV === 'local') {
+    credential = admin.credential.cert(serviceAccount)
+} else if (process.env.NODE_ENV === 'production') {
+    credential = admin.credential.applicationDefault()
+}
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
+
+const db = admin.firestore();
+
 const routersInit = config => {
 
     const router = express();
