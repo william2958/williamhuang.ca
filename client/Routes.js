7722b3bf96158824d91bc2c552a2ecfd6f68f877
William Huang <william2958@gmail.com>
2021-07-11 19:15:29 -0400

    feat: changes to set default view

--- client/Routes.js
+++ client/Routes.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import App from './App';
-import HomePage from './src/containers/HomePage';
+import App from "./App";
+import HomePage from "./src/containers/HomePage";
 
 import BookReviews from "./src/containers/BookReviews";
-import BookReviewDetail from './src/containers/BookReviews/BookReviewDetail';
+import BookReviewDetail from "./src/containers/BookReviews/BookReviewDetail";
 
-import Projects from './src/containers/Projects';
+import Projects from "./src/containers/Projects";
 
-import Error404Page from './src/containers/Error404Page';
+import Error404Page from "./src/containers/Error404Page";
 
-import PublicWrapper from './src/containers/PublicWrapper';
+import PublicWrapper from "./src/containers/PublicWrapper";
 import AdminLogin from "./src/containers/Admin/AdminLogin";
 import Admin from "./src/containers/Admin/Admin";
 import AdminHome from "./src/containers/Admin/AdminHome";
@@ -19,8 +19,8 @@ import BookReviewDrafts from "./src/containers/Admin/BookReviews/BookReviewDraft
 import NewBookReview from "./src/containers/Admin/BookReviews/NewBookReview";
 import EditBookReview from "./src/containers/Admin/BookReviews/EditBookReview";
 
-import GuidesPage from './src/containers/Guides';
-import GuideDetail from './src/containers/Guides/GuideDetail';
+import GuidesPage from "./src/containers/Guides";
+import GuideDetail from "./src/containers/Guides/GuideDetail";
 import PublishedGuides from "./src/containers/Admin/Guides/PublishedGuides";
 import DraftGuides from "./src/containers/Admin/Guides/DraftGuides";
 import NewGuide from "./src/containers/Admin/Guides/NewGuide";
@@ -29,7 +29,7 @@ import EditProject from "./src/containers/Admin/Projects/EditProject";
 import NewProject from "./src/containers/Admin/Projects/NewProject";
 import DraftProjects from "./src/containers/Admin/Projects/DraftProjects";
 import PublishedProjects from "./src/containers/Admin/Projects/PublishedProjects";
-import ProjectDetail from './src/containers/Projects/ProjectDetail';
+import ProjectDetail from "./src/containers/Projects/ProjectDetail";
 import PublishedEvents from "./src/containers/Admin/Events/PublishedEvents";
 import DraftEvents from "./src/containers/Admin/Events/DraftEvents";
 import NewEvent from "./src/containers/Admin/Events/NewEvent";
@@ -38,235 +38,235 @@ import PublishedMonthlyFives from "./src/containers/Admin/MonthlyFives/Published
 import DraftMonthlyFives from "./src/containers/Admin/MonthlyFives/DraftMonthlyFives";
 import NewMonthlyFive from "./src/containers/Admin/MonthlyFives/NewMonthlyFive";
 import EditMonthlyFive from "./src/containers/Admin/MonthlyFives/EditMonthlyFive";
-import EventDetail from './src/containers/Events/EventDetail';
-import EventsPage from './src/containers/Events';
-import MonthlyFiveDetail from './src/containers/MonthlyFives/MonthlyFiveDetail';
-import MonthlyFivesPage from './src/containers/MonthlyFives';
+import EventDetail from "./src/containers/Events/EventDetail";
+import EventsPage from "./src/containers/Events";
+import MonthlyFiveDetail from "./src/containers/MonthlyFives/MonthlyFiveDetail";
+import MonthlyFivesPage from "./src/containers/MonthlyFives";
 import PublishedBlogs from "./src/containers/Admin/Blogs/PublishedBlogs";
 import DraftBlogs from "./src/containers/Admin/Blogs/DraftBlogs";
 import NewBlog from "./src/containers/Admin/Blogs/NewBlog";
 import EditBlog from "./src/containers/Admin/Blogs/EditBlog";
-import BlogsPage from './src/containers/Blogs';
-import BlogDetail from './src/containers/Blogs/BlogDetail';
+import BlogsPage from "./src/containers/Blogs";
+import BlogDetail from "./src/containers/Blogs/BlogDetail";
 
 export default [
-	{
-		...App,
-		routes: [
-			{
-				...Admin,
-				path: '/admin',
-				routes: [
-					{
-						...AdminHome,
-						path: '/admin',
-						exact: true
-					},
-					{
-						...BookReviewPublished,
-						path: '/admin/bookReview/published'
-					},
-					{
-						...BookReviewDrafts,
-						path: '/admin/bookReview/drafts'
-					},
-					{
-						...NewBookReview,
-						path: '/admin/bookReview/new'
-					},
-					{
-						...EditBookReview,
-						path: '/admin/bookReview/edit/:reviewId'
-					},
+  {
+    ...App,
+    routes: [
+      {
+        ...Admin,
+        path: "/admin",
+        routes: [
+          {
+            ...AdminHome,
+            path: "/admin",
+            exact: true,
+          },
+          {
+            ...BookReviewPublished,
+            path: "/admin/bookReview/published",
+          },
+          {
+            ...BookReviewDrafts,
+            path: "/admin/bookReview/drafts",
+          },
+          {
+            ...NewBookReview,
+            path: "/admin/bookReview/new",
+          },
+          {
+            ...EditBookReview,
+            path: "/admin/bookReview/edit/:reviewId",
+          },
 
-					{
-						...PublishedGuides,
-						path: '/admin/guide/published'
-					},
-					{
-						...DraftGuides,
-						path: '/admin/guide/drafts'
-					},
-					{
-						...NewGuide,
-						path: '/admin/guide/new'
-					},
-					{
-						...EditGuide,
-						path: '/admin/guide/edit/:guideId'
-					},
+          {
+            ...PublishedGuides,
+            path: "/admin/guide/published",
+          },
+          {
+            ...DraftGuides,
+            path: "/admin/guide/drafts",
+          },
+          {
+            ...NewGuide,
+            path: "/admin/guide/new",
+          },
+          {
+            ...EditGuide,
+            path: "/admin/guide/edit/:guideId",
+          },
 
-					{
-						...PublishedProjects,
-						path: '/admin/project/published'
-					},
-					{
-						...DraftProjects,
-						path: '/admin/project/drafts'
-					},
-					{
-						...NewProject,
-						path: '/admin/project/new'
-					},
-					{
-						...EditProject,
-						path: '/admin/project/edit/:projectId'
-					},
+          {
+            ...PublishedProjects,
+            path: "/admin/project/published",
+          },
+          {
+            ...DraftProjects,
+            path: "/admin/project/drafts",
+          },
+          {
+            ...NewProject,
+            path: "/admin/project/new",
+          },
+          {
+            ...EditProject,
+            path: "/admin/project/edit/:projectId",
+          },
 
-					{
-						...PublishedEvents,
-						path: '/admin/event/published'
-					},
-					{
-						...DraftEvents,
-						path: '/admin/event/drafts'
-					},
-					{
-						...NewEvent,
-						path: '/admin/event/new'
-					},
-					{
-						...EditEvent,
-						path: '/admin/event/edit/:eventId'
-					},
+          {
+            ...PublishedEvents,
+            path: "/admin/event/published",
+          },
+          {
+            ...DraftEvents,
+            path: "/admin/event/drafts",
+          },
+          {
+            ...NewEvent,
+            path: "/admin/event/new",
+          },
+          {
+            ...EditEvent,
+            path: "/admin/event/edit/:eventId",
+          },
 
-					{
-						...PublishedMonthlyFives,
-						path: '/admin/monthlyFive/published'
-					},
-					{
-						...DraftMonthlyFives,
-						path: '/admin/monthlyFive/drafts'
-					},
-					{
-						...NewMonthlyFive,
-						path: '/admin/monthlyFive/new'
-					},
-					{
-						...EditMonthlyFive,
-						path: '/admin/monthlyFive/edit/:monthlyFiveId'
-					},
+          {
+            ...PublishedMonthlyFives,
+            path: "/admin/monthlyFive/published",
+          },
+          {
+            ...DraftMonthlyFives,
+            path: "/admin/monthlyFive/drafts",
+          },
+          {
+            ...NewMonthlyFive,
+            path: "/admin/monthlyFive/new",
+          },
+          {
+            ...EditMonthlyFive,
+            path: "/admin/monthlyFive/edit/:monthlyFiveId",
+          },
 
-					{
-						...PublishedBlogs,
-						path: '/admin/blog/published'
-					},
-					{
-						...DraftBlogs,
-						path: '/admin/blog/drafts'
-					},
-					{
-						...NewBlog,
-						path: '/admin/blog/new'
-					},
-					{
-						...EditBlog,
-						path: '/admin/blog/edit/:blogId'
-					}
-				]
-			},
-			{
-				...PublicWrapper,
-				routes: [
-					{
-						...HomePage,
-						path: '/',
-						exact: true
-					},
+          {
+            ...PublishedBlogs,
+            path: "/admin/blog/published",
+          },
+          {
+            ...DraftBlogs,
+            path: "/admin/blog/drafts",
+          },
+          {
+            ...NewBlog,
+            path: "/admin/blog/new",
+          },
+          {
+            ...EditBlog,
+            path: "/admin/blog/edit/:blogId",
+          },
+        ],
+      },
+      {
+        ...PublicWrapper,
+        routes: [
+          // Remove this duplicated path
+          {
+            ...AdminLogin,
+            path: "/adminLogin",
+          },
+          {
+            ...HomePage,
+            path: "/",
+            // exact: true
+          },
 
+          {
+            ...BookReviewDetail,
+            path: "/bookReviews/id/:id",
+          },
+          {
+            ...BookReviewDetail,
+            path: "/bookReviews/:urlString",
+          },
+          {
+            ...BookReviews,
+            path: "/bookReviews",
+          },
 
-					{
-						...BookReviewDetail,
-						path: '/bookReviews/id/:id'
-					},
-					{
-						...BookReviewDetail,
-						path: '/bookReviews/:urlString'
-					},
-					{
-						...BookReviews,
-						path: '/bookReviews'
-					},
+          {
+            ...ProjectDetail,
+            path: "/projects/id/:id",
+          },
+          {
+            ...ProjectDetail,
+            path: "/projects/:urlString",
+          },
+          {
+            ...Projects,
+            path: "/projects",
+          },
 
+          {
+            ...GuideDetail,
+            path: "/guides/id/:id",
+          },
+          {
+            ...GuideDetail,
+            path: "/guides/:urlString",
+          },
+          {
+            ...GuidesPage,
+            path: "/guides",
+          },
 
-					{
-						...ProjectDetail,
-						path: '/projects/id/:id'
-					},
-					{
-						...ProjectDetail,
-						path: '/projects/:urlString'
-					},
-					{
-						...Projects,
-						path: '/projects'
-					},
+          {
+            ...EventDetail,
+            path: "/events/id/:id",
+          },
+          {
+            ...EventDetail,
+            path: "/events/:urlString",
+          },
+          {
+            ...EventsPage,
+            path: "/events",
+          },
 
+          {
+            ...BlogDetail,
+            path: "/blog/id/:id",
+          },
+          {
+            ...BlogDetail,
+            path: "/blog/:urlString",
+          },
+          {
+            ...BlogsPage,
+            path: "/blog",
+          },
 
-					{
-						...GuideDetail,
-						path: '/guides/id/:id'
-					},
-					{
-						...GuideDetail,
-						path: '/guides/:urlString'
-					},
-					{
-						...GuidesPage,
-						path: '/guides'
-					},
+          {
+            ...MonthlyFiveDetail,
+            path: "/monthlyBullets/id/:id",
+          },
+          {
+            ...MonthlyFiveDetail,
+            path: "/monthlyBullets/:urlString",
+          },
+          {
+            ...MonthlyFivesPage,
+            path: "/monthlyBullets",
+          },
 
+          {
+            ...AdminLogin,
+            path: "/adminLogin",
+          },
 
-					{
-						...EventDetail,
-						path: '/events/id/:id'
-					},
-					{
-						...EventDetail,
-						path: '/events/:urlString'
-					},
-					{
-						...EventsPage,
-						path: '/events'
-					},
-
-					{
-						...BlogDetail,
-						path: '/blog/id/:id'
-					},
-					{
-						...BlogDetail,
-						path: '/blog/:urlString'
-					},
-					{
-						...BlogsPage,
-						path: '/blog'
-					},
-
-
-					{
-						...MonthlyFiveDetail,
-						path: '/monthlyBullets/id/:id'
-					},
-					{
-						...MonthlyFiveDetail,
-						path: '/monthlyBullets/:urlString'
-					},
-					{
-						...MonthlyFivesPage,
-						path: '/monthlyBullets'
-					},
-
-					{
-						...AdminLogin,
-						path: '/adminLogin'
-					},
-
-					{
-						...Error404Page
-					}
-				]
-			}
-		]
-	}
-]
+          {
+            ...Error404Page,
+          },
+        ],
+      },
+    ],
+  },
+];
--- client/src/containers/HomePage/index.js
+++ client/src/containers/HomePage/index.js
@@ -1,70 +1,68 @@
-import React from 'react';
+import React from "react";
 
-import {HomePageWrapper} from "./styles";
+import { HomePageWrapper } from "./styles";
 import Header from "../../components/Header";
 import RecentBookReviews from "./components/RecentBookReviews";
 import {
-    getFirstPageBlogs,
-    getNumRecentBookReviews,
-    getNumRecentGuides,
-    getNumRecentMonthlyFives,
-    getNumRecentProjects
+  getFirstPageBlogs,
+  getNumRecentBookReviews,
+  getNumRecentGuides,
+  getNumRecentMonthlyFives,
+  getNumRecentProjects,
 } from "../../actions";
 import RecentGuides from "./components/RecentGuides";
 import RecentProjects from "./components/RecentProjects";
-import {getHomeHighlight} from "../../actions/homeActions";
+import { getHomeHighlight } from "../../actions/homeActions";
 import HomePageBanner from "./components/HomePageBanner";
 import RecentMonthlyFive from "./components/RecentMonthlyFive";
-import {Helmet} from "react-helmet";
-import {ThemeProvider} from "styled-components";
+import { Helmet } from "react-helmet";
+import { ThemeProvider } from "styled-components";
 import RecentBlogs from "./components/RecentBlogs";
 
 class HomePage extends React.Component {
+  componentDidMount() {
+    if (typeof window !== "undefined") window.scrollTo(0, 0);
+  }
 
-    componentDidMount() {
-        if (typeof window !== 'undefined') window.scrollTo(0, 0);
-    }
-
-    render() {
-        return (
-            <HomePageWrapper>
-                <Helmet>
-                    <title>{"Home | WH"}</title>
-                    <meta property="og:title" content="Home | williamhuang.ca" />
-                    <meta property="og:image" content="https://storage.googleapis.com/global_images/Web_Preview/favicon.png" />
-                    <meta property="description" content="Explore my blog, reviews, guides, and more." />
-                    <meta property="og:url" content="https://williamhuang.ca" />
-                </Helmet>
-
-                <HomePageBanner />
-                <Header sticky />
-                <RecentBlogs />
-                <RecentMonthlyFive />
-                {/*<RecentBookReviews />*/}
-
-                <RecentGuides />
-            </HomePageWrapper>
-        )
-    }
-
+  render() {
+    return <div>williamhuang.ca</div>;
+    // return (
+    //     <HomePageWrapper>
+    //         <Helmet>
+    //             <title>{"Home | WH"}</title>
+    //             <meta property="og:title" content="Home | williamhuang.ca" />
+    //             <meta property="og:image" content="https://storage.googleapis.com/global_images/Web_Preview/favicon.png" />
+    //             <meta property="description" content="Explore my blog, reviews, guides, and more." />
+    //             <meta property="og:url" content="https://williamhuang.ca" />
+    //         </Helmet>
+    //
+    //         <HomePageBanner />
+    //         <Header sticky />
+    //         <RecentBlogs />
+    //         <RecentMonthlyFive />
+    //         {/*<RecentBookReviews />*/}
+    //
+    //         <RecentGuides />
+    //     </HomePageWrapper>
+    // )
+  }
 }
 
 function loadData(store) {
-    return Promise.all([
-        store.dispatch(getNumRecentBookReviews(4)),
-        store.dispatch(getNumRecentGuides(4)),
-        store.dispatch(getNumRecentProjects(3)),
-        store.dispatch(getHomeHighlight()),
-        store.dispatch(getNumRecentMonthlyFives(3)),
-        store.dispatch(getFirstPageBlogs())
-    ])
+  return Promise.all([
+    store.dispatch(getNumRecentBookReviews(4)),
+    store.dispatch(getNumRecentGuides(4)),
+    store.dispatch(getNumRecentProjects(3)),
+    store.dispatch(getHomeHighlight()),
+    store.dispatch(getNumRecentMonthlyFives(3)),
+    store.dispatch(getFirstPageBlogs()),
+  ]);
 }
 
 export default {
-    component: HomePage,
-    loadData
+  component: HomePage,
+  loadData,
 };
-
 
 //
 // {/*<HomePageSection className="container">*/}
